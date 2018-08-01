b73f30a395688a56fd790d016e10d6d458b32c67
Jack Draak <jackdraak@gmail.com>
2018-08-01 02:08:44 -0400

    playing with player control, continued. (added thrust tuner).

--- CU2_Boost2018/Assets/_Scripts/Player.cs
+++ CU2_Boost2018/Assets/_Scripts/Player.cs
@@ -5,68 +5,165 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-   [SerializeField] int rotationFactor = 300;
-   [SerializeField] int thrustFactor = 500;
-   [SerializeField] float thrustVolume = 0.66f;
-   [SerializeField] float deR = 0.66f;
-   [SerializeField] float deL = 0.66f;
    [SerializeField] float masterVolume = 1.0f;
+   [SerializeField] int rotationFactor = 300;
    [SerializeField] AudioClip thrustSound;
+   [SerializeField] float thrustVolume = 0.42f;
+   public float tSliderValue = 500.0f;
 
-   private Vector2 controlAxis = Vector2.zero;
-
-   ParticleSystem thrustParticles;
-   Rigidbody rigidbody;
-   RigidbodyConstraints constraints;
-   Vector3 localEulers;
-
-   AudioSource audioSource;
+   private AudioSource audioSource;
+   private bool derot;
+   private float derotTime;
+   private float tEmissionRate = 113.0f;
+   private float tNonEmissionRate = 13.0f;
+   private float tSliderMax = 800.0f;
+   private float tSliderMin = 200.0f;
+   private ParticleSystem thrustParticles;
+   private Rigidbody rigidbody;
+   private RigidbodyConstraints constraints;
+   private Vector3 controlAxis = Vector3.zero;
+   private Vector3 localEulers;
 
    void Start ()
    {
-      rigidbody = GetComponent<Rigidbody>();
+      audioSource = GetComponent<AudioSource>();
       thrustParticles = GetComponent<ParticleSystem>();
+      rigidbody = GetComponent<Rigidbody>();
+
+      derot = false;
+      var emission = thrustParticles.emission;
+      emission.rateOverTime = tNonEmissionRate;
 
       rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | 
          RigidbodyConstraints.FreezeRotationX | 
          RigidbodyConstraints.FreezeRotationY |
          RigidbodyConstraints.FreezeRotationZ;
-      constraints = rigidbody.constraints;
 
-      audioSource = GetComponent<AudioSource>();
+      constraints = rigidbody.constraints;
    }
-	
-	void Update ()
+
+   void LateUpdate ()
    {
 		ProcessInput();
 	}
 
-   private void ProcessInput()
+   private void AdjustThrust(float delta)
    {
-      controlAxis.x = CrossPlatformInputManager.GetAxis("Horizontal");
-      if (controlAxis.x != 0) Rotate(controlAxis.x);
-      else DeRotate();
-
-      controlAxis.y = CrossPlatformInputManager.GetAxis("Jump");
-      if (controlAxis.y != 0) Thrust(controlAxis.y);
-      else if (audioSource.isPlaying)
+      if (delta + tSliderValue > tSliderMax)
       {
-         audioSource.Stop();
-         thrustParticles.emissionRate = 0;
+         tSliderValue = tSliderMax;
+      }
+      else if (delta + tSliderValue < tSliderMin)
+      {
+         tSliderValue = tSliderMin;
+      }
+      else tSliderValue += delta;
+   }
+
+   private void CounterRotate()
+   {
+      float speed = Mathf.Abs(Time.time - derotTime) / 3;
+      localEulers = transform.localRotation.eulerAngles;
+      var important = localEulers.z;
+      if (important >= 180 && important < 359)
+      {
+         transform.Rotate(Vector3.forward * (important * speed) * Time.deltaTime);
+      }
+      else if (important < 180 && important > 1)
+      {
+         transform.Rotate(Vector3.back * ((important + 180) * speed) * Time.deltaTime);
       }
    }
 
    private void DeRotate()
    {
-      localEulers = transform.localRotation.eulerAngles;
-      var important = localEulers.z;
-      if (important >= 180 && important < 359)
+      if (!derot)
       {
-         transform.Rotate(Vector3.forward * (important * deR) * Time.fixedDeltaTime);
+         derotTime = Time.time;
+         derot = true;
       }
-      else if (important < 180 && important > 1)
+      else
       {
-         transform.Rotate(Vector3.back * ((important + 180) * deL) * Time.fixedDeltaTime);
+         CounterRotate();
+      }
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+      switch (collision.gameObject.tag)
+      {
+         case "GoodObject_01":
+            //Debug.Log("collision: GO-01");
+            break;
+         case "GoodObject_02":
+            //Debug.Log("collision: GO-02");
+            break;
+         case "BadObject_01":
+            //Debug.Log("collision: BO-01");
+            break;
+         case "BadObject_02":
+            //Debug.Log("collision: BO-02");
+            break;
+         default:
+            //Debug.Log("collision: default");
+            break;
+      }
+   }
+
+   void OnGUI()
+   {
+      int r_x, r_y, r_w, r_h;
+      r_x = 35;
+      r_y = 45;
+      r_w = 100;
+      r_h = 30;
+      Rect sliderRect = new Rect(r_x, r_y, r_w, r_h);
+      Rect labelRect = new Rect(r_x, r_y + r_h, r_w * 3, r_h);
+
+      tSliderValue = GUI.HorizontalSlider(sliderRect, tSliderValue, tSliderMin, tSliderMax);
+      GUI.Label(labelRect, "Thruster Power Control (Up/Down Keys)");
+   }
+
+   private void ProcessInput()
+   {
+      Pro_Horizontal();
+      Pro_Vertical();
+      Pro_Action();
+   }
+
+   private void Pro_Action()
+   {
+      var emission = thrustParticles.emission;
+      controlAxis.z = CrossPlatformInputManager.GetAxis("Jump");
+      if (controlAxis.z != 0)
+      {
+         Thrust(controlAxis.z);
+         emission.rateOverTime = tEmissionRate;
+      }
+      else if (audioSource.isPlaying)
+      {
+         audioSource.Stop();
+         emission.rateOverTime = tNonEmissionRate;
+      }
+   }
+
+   private void Pro_Horizontal()
+   {
+      controlAxis.x = CrossPlatformInputManager.GetAxis("Horizontal");
+      if (controlAxis.x != 0)
+      {
+         Rotate(controlAxis.x);
+         derot = false;
+      }
+      else DeRotate();
+   }
+
+   private void Pro_Vertical()
+   {
+      controlAxis.y = CrossPlatformInputManager.GetAxis("Vertical");
+      if (controlAxis.y != 0)
+      {
+         AdjustThrust(controlAxis.y);
       }
    }
 
@@ -80,8 +177,7 @@ public class Player : MonoBehaviour {
 
    private void Thrust(float force)
    {
-      rigidbody.AddRelativeForce(Vector3.up * thrustFactor * Time.deltaTime * force);
-      thrustParticles.emissionRate = 100;
+      rigidbody.AddRelativeForce(Vector3.up * tSliderValue * Time.deltaTime * force);
       if (!audioSource.isPlaying) audioSource.PlayOneShot(thrustSound, thrustVolume * masterVolume);
    }
 }
