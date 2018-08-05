1f78ad8791cc472337106cf2c0705fa39ce4895d
Jack Draak <jackdraak@gmail.com>
2018-08-05 03:22:13 -0400

    diving into lots of features... fuel, collisions, effects, HUD...

--- CU2_Boost2018/Assets/_Scripts/Player.cs
+++ CU2_Boost2018/Assets/_Scripts/Player.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityStandardAssets.CrossPlatformInput;
+﻿using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -10,40 +9,56 @@ public class Player : MonoBehaviour {
    [SerializeField] int rotationFactor = 300;
    [SerializeField] int thrustFactor = 10;
    [SerializeField] AudioClip thrustSound;
+   [SerializeField] AudioClip bonusSound;
+   [SerializeField] GameObject thrustLight;
    [SerializeField] GameObject tutorialText;
+   [SerializeField] Text FuelReadout;
+   [SerializeField] GameObject collisionEffect;
 
    private bool debugMode, deRotating, invulnerable, tutorialVisible;
    private float deRotationTime;
+   private float currentEmissionRate;
+   private float fuelGenRate = 50;
+   private float fuelLevel = 1000f;
+   private float fuelMax = 1000f;
+   private float fuelUseRate = 75;
    private float thrustAudioLength;
    private float thrustAudioTimer;
    private float thrustEmissionRate = 60f;
+   private float rotationEmissionRate = 20f;
    private float thrustNonEmissionRate = 1.3f;
    private float thrustSliderMax = 120f;
    private float thrustSliderMin = 25f;
    private float thrustSliderValue = 45f;
-   private float thrustMax = 0;
+   private float thrustMax = 0f;
    private Vector3 threeControlAxis = Vector3.zero;
    private Vector3 thrustState = Vector3.zero;
    private Vector3 localEulers = Vector3.zero;
    private AudioSource audioSource;
    private ParticleSystem thrustParticleSystem;
    private ParticleSystem.EmissionModule emission;
+   private ParticleSystem collisionParticleSystem;
+   private ParticleSystem.EmissionModule colmission;
    private Rigidbody thisRigidbody;
    private RigidbodyConstraints rigidbodyConstraints;
 
    void Start ()
    {
+      thrustLight.SetActive(false);
       debugMode = Debug.isDebugBuild;
-
       audioSource = GetComponent<AudioSource>();
       thisRigidbody = GetComponent<Rigidbody>();
       thrustParticleSystem = GetComponent<ParticleSystem>();
+      collisionParticleSystem = collisionEffect.GetComponent<ParticleSystem>();
+      colmission = collisionParticleSystem.emission;
 
       emission = thrustParticleSystem.emission;
-      emission.rateOverTime = thrustNonEmissionRate;
+      currentEmissionRate = thrustNonEmissionRate;
+      emission.rateOverTime = currentEmissionRate;
       thrustAudioLength = thrustSound.length;
       thrustAudioTimer -= thrustAudioLength;
 
+      FuelReadout.text = fuelLevel.ToString();
       deRotating = false;
       invulnerable = false;
       tutorialVisible = true;
@@ -56,8 +71,10 @@ public class Player : MonoBehaviour {
       rigidbodyConstraints = thisRigidbody.constraints;
    }
 
-   void FixedUpdate ()
+   void FixedUpdate () // Previously, FixedUpdate didnt seem to address surigng issue; Now it's definately required for smooth 'flight'.
    {
+      GenerateFuel();
+      FuelReadout.text = fuelLevel.ToString();
       if (debugMode) DebugControlPoll();
 		PlayerControlPoll();
    }
@@ -90,10 +107,35 @@ public class Player : MonoBehaviour {
       }
    }
 
+   private bool BurnGas()
+   {
+      float burnRate = fuelUseRate * Time.fixedDeltaTime;
+      if (fuelLevel > burnRate)
+      {
+         fuelLevel -= burnRate;
+         return true;
+      }
+      else
+      {
+         fuelLevel = 0;
+         return false;
+      }
+   }
+
    private void DebugControlPoll()
    {
       if (Input.GetKeyDown(KeyCode.I)) invulnerable = !invulnerable;
       if (Input.GetKeyDown(KeyCode.R)) thrustMax = 0;
+   }
+
+   private void EndBurn()
+   {
+      audioSource.Stop();
+      thrustAudioTimer -= thrustAudioLength;
+      currentEmissionRate = thrustNonEmissionRate;
+      emission.rateOverTime = currentEmissionRate;
+      thrustState = Vector3.zero;
+      thrustLight.SetActive(false);
    }
 
    private void DeRotate()
@@ -105,8 +147,16 @@ public class Player : MonoBehaviour {
       }
       else
       {
+         currentEmissionRate = thrustNonEmissionRate;
+         emission.rateOverTime = currentEmissionRate;
          AutoDeRotate();
       }
+   }
+
+   private void GenerateFuel()
+   {
+      fuelLevel += Time.fixedDeltaTime * fuelGenRate;
+      if (fuelLevel > fuelMax) fuelLevel = fuelMax;
    }
 
    private void HideTutorial()
@@ -120,14 +170,47 @@ public class Player : MonoBehaviour {
       switch (collision.gameObject.tag)
       {
          case "BadObject_01":
-            if (!invulnerable) Debug.Log("collision: BO-01");
+            if (!invulnerable)
+            {
+               //Debug.Log("collision: BO-01");
+               fuelLevel -= 100;
+               if (fuelLevel < 0) fuelLevel = 0;
+               GameObject newDamage = (GameObject)Instantiate(collisionEffect, transform.position, Quaternion.identity);
+               Destroy(newDamage, 1f);
+            }
             else Debug.Log("invulnerable: BO-01");
             break;
          case "BadObject_02":
             //Debug.Log("collision: BO-02");
             break;
          case "GoodObject_01":
-            //Debug.Log("collision: GO-01");
+            Debug.Log("Pickup Object");
+            break;
+         case "GoodObject_02":
+            //Debug.Log("collision: GO-02");
+            break;
+         default:
+            //Debug.Log("collision: default");
+            break;
+      }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      switch (other.gameObject.tag)
+      {
+         case "BadObject_01":
+            Debug.Log("trigger: BO-01");
+            break;
+         case "BadObject_02":
+            //Debug.Log("collision: BO-02");
+            break;
+         case "GoodObject_01":
+            other.gameObject.SetActive(false);
+            audioSource.PlayOneShot(bonusSound, 0.5f * masterVolume);
+            fuelLevel += 500;
+            if (fuelLevel > fuelMax) fuelLevel = fuelMax;
+            //Debug.Log("Pickup Object Trigger");
             break;
          case "GoodObject_02":
             //Debug.Log("collision: GO-02");
@@ -147,12 +230,12 @@ public class Player : MonoBehaviour {
       r_h = 30;
       Rect sliderRect = new Rect(r_x, r_y, r_w, r_h);
       Rect labelRect = new Rect(r_x, r_y + r_h, r_w * 3, r_h);
-      Rect thrustRect = new Rect(r_x, r_y + r_h * 2 , r_w * 3, r_h);
+      Rect thrustRect = new Rect(r_x, r_y + r_h * 2 , r_w * 3, r_h *2);
 
       thrustSliderValue = GUI.HorizontalSlider(sliderRect, thrustSliderValue, thrustSliderMin, thrustSliderMax);
       if (thrustState.y > thrustMax) thrustMax = thrustState.y;
       GUI.Label(labelRect, "<color=\"#FF7070\"><b><i>Thruster Power Control</i></b> (Up/Down Keys)</color>");
-      GUI.Label(thrustRect, "<color=\"#FF7070\"><b>" + thrustState.y + "<p>" + thrustMax + "</b></color>");
+      if (debugMode) GUI.Label(thrustRect, "<color=\"#FF7070\"><b>T-Power: current/peak\n" + thrustState.y + "\n" + thrustMax + "</b></color>");
    }
 
    private void PlayerControlPoll()
@@ -165,22 +248,22 @@ public class Player : MonoBehaviour {
 
    private void PollAction()
    {
-      bool thrust = Input.GetKey(KeyCode.Space);
-      if (thrust)
-      //threeControlAxis.z = CrossPlatformInputManager.GetAxis("Jump");
-      //if (threeControlAxis.z != 0)
+      threeControlAxis.z = CrossPlatformInputManager.GetAxis("Jump");
+      if (threeControlAxis.z != 0)
       {
          if (tutorialVisible) HideTutorial();
-         Thrust(1f);
-         //Thrust(threeControlAxis.z);
-         emission.rateOverTime = thrustEmissionRate;
+         if (BurnGas())
+         {
+            Thrust(threeControlAxis.z);
+            currentEmissionRate = thrustEmissionRate;
+            emission.rateOverTime = currentEmissionRate;
+            thrustLight.SetActive(true);
+         }
+         else EndBurn();
       }
       else if (audioSource.isPlaying)
       {
-         audioSource.Stop();
-         thrustAudioTimer -= thrustAudioLength;
-         emission.rateOverTime = thrustNonEmissionRate;
-         thrustState = Vector3.zero;
+         EndBurn();
       }
    }
 
@@ -190,8 +273,11 @@ public class Player : MonoBehaviour {
       if (threeControlAxis.x != 0)
       {
          if (tutorialVisible) HideTutorial();
-         Rotate(threeControlAxis.x);
-         deRotating = false;
+         if (BurnGas())
+         {
+            Rotate(threeControlAxis.x);
+            deRotating = false;
+         }
       }
       else DeRotate();
    }
@@ -217,14 +303,17 @@ public class Player : MonoBehaviour {
       thisRigidbody.constraints = RigidbodyConstraints.None;
       transform.Rotate(Vector3.back * rotationFactor * Time.fixedDeltaTime * direction);
       thisRigidbody.constraints = rigidbodyConstraints;
+      if (currentEmissionRate < rotationEmissionRate)
+      {
+         currentEmissionRate = rotationEmissionRate;
+         emission.rateOverTime = currentEmissionRate;
+      }
    }
 
    private void Thrust(float force)
    {
-      thrustState = Vector3.up * thrustSliderValue * thrustFactor * Time.smoothDeltaTime * force;
-      if (thrustState.y > 4 * thrustSliderValue) thrustState.y = 4 * thrustSliderValue;
-      //TextEditor myText = tutorialText.GetComponent<TextEditor>();
-      //myText = thisValue.ToString();
+      thrustState = Vector3.up * thrustSliderValue * thrustFactor * Time.deltaTime * force; // weird.. smoothDeltaTime seemed to fix surging, but now it doesnt seem needed!?!?
+      //if (thrustState.y > 4 * thrustSliderValue) thrustState.y = 4 * thrustSliderValue;
 
       thisRigidbody.AddRelativeForce(thrustState);
       // if the audio clip is in the final half second, re-que it
---
+++ CU2_Boost2018/Assets/_Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+   [SerializeField] float spinRate = 0.1f;
+
+   private void Update()
+   {
+      transform.Rotate(0, Time.time * spinRate, 0, Space.World);
+   }
+
+}
