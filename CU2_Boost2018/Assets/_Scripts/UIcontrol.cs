11604154c879a108d4abbe5440382530a6ee2403
Jack Draak <jackdraak@gmail.com>
2018-08-25 16:20:35 -0400

    more generic

--- CU2_Boost2018/Assets/_Scripts/UIcontrol.cs
+++ CU2_Boost2018/Assets/_Scripts/UIcontrol.cs
@@ -3,16 +3,11 @@
 public class UIcontrol : MonoBehaviour {
 
    // UI elements to hide when the game is: launched | paused | reset 
-   [SerializeField] GameObject GL_slider;
-   [SerializeField] GameObject MGP_slider;
-   [SerializeField] GameObject TPC_slider;
-   [SerializeField] GameObject TPL_slider;
-   [SerializeField] GameObject tasklist;
-   private GameObject[] UIobjects = new GameObject[5];
+   [SerializeField] GameObject[] UIobjects;
 
    private bool deltaStatus, status;
 
-   public bool HUD_visible
+   public bool visible
    {
       get { return status; }
       set { deltaStatus = value; }
@@ -26,12 +21,6 @@ public class UIcontrol : MonoBehaviour {
 
    private void Start()
    {
-      UIobjects[0] = TPC_slider;
-      UIobjects[1] = TPL_slider;
-      UIobjects[2] = GL_slider;
-      UIobjects[3] = MGP_slider;
-      UIobjects[4] = tasklist;
-
       // pre-emtively deactivate the controlled HUD elements...
       deltaStatus = false;
       status = false;
