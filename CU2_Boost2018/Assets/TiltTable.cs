952bbf24a309761cf887187a16ebb08081b2dfa0
Jack Draak <jackdraak@gmail.com>
2018-11-20 06:49:29 -0500

    MouseGimbal / TiltTable - functional

--- CU2_Boost2018/Assets/TiltTable.cs
+++ CU2_Boost2018/Assets/TiltTable.cs
@@ -2,37 +2,48 @@
 
 public class TiltTable : MonoBehaviour
 {
+   [SerializeField] [Range(0,89)] float maxRotationDegrees = 10.0f; // At 90+ gimbal oddities must be dealt with.
+   [SerializeField] bool ClampToMaxRotationDegrees = true;
    [SerializeField] float rotationSpeed = 10.0f;
-   [SerializeField] float minRotation = -10.0f;
-   [SerializeField] float maxRotation = 10.0f;
 
-   void Update ()
+   const float fullArc = 360.0f;
+   const float halfArc = 180.0f;
+   const float nullArc = 0.0f;
+
+   void Update () { tiltTable(); }
+
+   void tiltTable()
    {
-      tiltTable();
+      // Apply the 'pre-clamp' rotation (rotation-Z and rotation-X from X & Y of mouse, respectively).
+      if (maxRotationDegrees > 0) { SimpleRotation(GetMouseInput()); }
+
+      // Clamp rotation to maxRotationDegrees.
+      if (ClampToMaxRotationDegrees) { ClampRotation(transform.rotation.eulerAngles); }
    }
 
-   private void tiltTable()
+   void ClampRotation(Vector3 tempEulers)
    {
-      SimpleRotation(GetMouseInput()); // this is the 'pre-clamp' rotation (rotation-Z and rotation-X, respectively from X & Y of mouse)
-      ClampRotation(transform.rotation.eulerAngles); // half-broken attempt to clamp rotation to min/maxRotation degrees.
-      /// moving left and/or up on mouse results in smooth roll into 10 degreee limits...
-      /// moving right and/or down on mouse eventually results in a sudden 'reset' to a nominal position... 
-      /// but why?
-   }
-
-   private void ClampRotation(Vector3 tempEulers)
-   {
-      tempEulers.x = Mathf.Clamp(tempEulers.x, minRotation, maxRotation);
-      tempEulers.y = Mathf.Clamp(tempEulers.y, minRotation, maxRotation);
-      tempEulers.z = Mathf.Clamp(tempEulers.z, minRotation, maxRotation);
+      tempEulers.x = ClampPlane(tempEulers.x);
+      tempEulers.z = ClampPlane(tempEulers.z);
+      tempEulers.y = 0.0f; // ClampPlane(tempEulers.y); // *See GIST note below...
       transform.rotation = Quaternion.Euler(tempEulers);
+      ///Debug.Log(tempEulers);
+   }
+
+   float ClampPlane(float plane)
+   {
+      if (OkayLow(plane) || OkayHigh(plane)) DoNothing(); // Plane 'in range'.
+      else if (BadLow(plane)) plane = Mathf.Clamp(plane, nullArc, maxRotationDegrees);
+      else if (BadHigh(plane)) plane = Mathf.Clamp(plane, fullArc - maxRotationDegrees, fullArc);
+      else Debug.LogWarning("WARN: invalid plane condition");
+      return plane;
    }
 
    Vector2 GetMouseInput()
    {
       Vector2 mouseXY;
-      mouseXY.x = -Input.GetAxis("Mouse X"); // rotZ
-      mouseXY.y = Input.GetAxis("Mouse Y"); // rotX
+      mouseXY.x = -Input.GetAxis("Mouse X"); // MouseX -> rotZ.
+      mouseXY.y = Input.GetAxis("Mouse Y"); // MouseY -> rotX.
       return mouseXY;
    }
 
@@ -43,4 +54,10 @@ public class TiltTable : MonoBehaviour
       rotation.z = mouseXY.x * Time.deltaTime * rotationSpeed;
       transform.Rotate(rotation, Space.Self); 
    }
+
+   void DoNothing()           {   }
+   bool OkayHigh(float test)  { return (test >= fullArc - maxRotationDegrees && test <= fullArc); }
+   bool OkayLow(float test)   { return (test >= nullArc && test <= maxRotationDegrees); }
+   bool BadHigh(float test)   { return (test > halfArc && !OkayHigh(test)); }
+   bool BadLow(float test)    { return (test < halfArc && !OkayLow(test)); }
 }
