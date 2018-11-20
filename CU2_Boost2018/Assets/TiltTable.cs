ad58cc0e911b8719ebf401476467e3ab172baa85
Jack Draak <jackdraak@gmail.com>
2018-11-20 01:12:39 -0500

    working on a marble-maze style tilt-table (mouse controlled)..

---
+++ CU2_Boost2018/Assets/TiltTable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TiltTable : MonoBehaviour
+{
+   [SerializeField] float rotationSpeed = 10.0f;
+   [SerializeField] float minRotation = -10.0f;
+   [SerializeField] float maxRotation = 10.0f;
+
+   Vector3 priorRotation = Vector3.zero;
+
+	void Update ()
+   {
+      var mouseX = -Input.GetAxis("Mouse X"); // rotZ
+      var mouseY = Input.GetAxis("Mouse Y"); // rotX
+
+      Vector3 rotation = new Vector3(mouseY, 0.0f, mouseX);
+      rotation.x = mouseY * Time.deltaTime * rotationSpeed;
+      rotation.z = mouseX * Time.deltaTime * rotationSpeed;
+
+      rotation.x += priorRotation.x;
+      rotation.z += priorRotation.z;
+
+      rotation.x = Mathf.Clamp(rotation.x, minRotation, maxRotation);
+      rotation.z = Mathf.Clamp(rotation.z, minRotation, maxRotation);
+
+      transform.Rotate(rotation, Space.Self);
+   }
+}
