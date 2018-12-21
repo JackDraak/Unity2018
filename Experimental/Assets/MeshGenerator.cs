0fb869fb254d4a9fd4e29579a89574ef2f68d5be
Jack Draak <jackdraak@gmail.com>
2018-12-20 19:29:06 -0500

    un-tidy https://www.youtube.com/watch?v=64NblGkAabk Brackey's PROCCEDURAL TERRAIN

--- Experimental/Assets/MeshGenerator.cs
+++ Experimental/Assets/MeshGenerator.cs
@@ -1,4 +1,4 @@
-﻿//using System.Collections;
+﻿using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,30 +10,57 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
+    public int xSize = 20;
+    public int zSize = 20;
+
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
-        UpdateMesh();
+        StartCoroutine(CreateShape());
     }
 
-    void CreateShape()
+    void Update()
     {
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0),
-            new Vector3(1,0,1)
-        };
+        UpdateMesh();   
+    }
 
-        triangles = new int[]
+    IEnumerator CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)]; 
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            0, 1, 2,
-            1, 3, 2
-        };
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int z = 0; z < zSize; z++)
+            {
+                triangles[tris] = vert;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return null;// new WaitForSeconds(0.1f);
+            }
+            vert++;
+        }      
     }
 
     void UpdateMesh()
@@ -43,5 +70,15 @@ public class MeshGenerator : MonoBehaviour
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }      
     }
 }
